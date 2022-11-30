a6e43244d66b42952c2d92a478d9a1e647e5714c
praveenmahasena647 <115216588+praveenmahasena647@users.noreply.github.com>
2022-11-30 20:51:48 +0530

    up to something

--- vue-assembly-ai/client/src/App.vue
+++ vue-assembly-ai/client/src/App.vue
@@ -1,17 +1,12 @@
-<script setup lang="ts">
-import { io } from "socket.io-client";
-import { ref } from "vue";
-let file = ref(null);
-
-async function getText() {
-}
+<script setup lang='ts'>
+async function getText() {}
 </script>
 
 <template>
-  <form @submit.prevent="getText">
+  <form @submit.prevent='getText'>
     select Your mp3 Clip
     <br />
-    <input type="file" ref="file" />
+    <input type='file' ref="file" />
     <br />
     <button>click to get result</button>
   </form>
